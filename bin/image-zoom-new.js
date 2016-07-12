bfb4eba02bf7bc604f275f0f1360c4b3bd6ba6e8
Sean Scanlan <sean.scanlan@kettlenyc.com>
2016-07-12 17:56:31 -0400

    Updating names

--- bin/image-zoom-new.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-var ImageZoomNew = require('./../src/image-zoom-new/image-zoom-new.js');
-var imageZoomNew = new ImageZoomNew();
--- src/lightbox/lightbox.js
+++ src/lightbox/lightbox.js
@@ -5,22 +5,21 @@ var toArray = require('to-array');
 var noScroll = require('no-scroll');
 
 /**
- * ImageZoomNew constructor description
+ * Lightbox constructor description
  *
- * @class ImageZoomNew
- * @classdesc ImageZoomNew class description
+ * @class Lightbox
+ * @classdesc Lightbox class description
  * This is an image zoomer. Like a lightbox, but prettier <3
  *
  * @param {String} imgStr - Query selector string for the images
  * @param {object} options - Instance instantiation object
  * @param {string} options.example - Example options property
  */
-function ImageZoomNew (options) {
-
-
+function Lightbox (options) {
+	console.log('lightbox!');
 }
 
-module.exports = ImageZoomNew;
+module.exports = Lightbox;
 
 /**
  * @function public
@@ -28,8 +27,8 @@ module.exports = ImageZoomNew;
  * @param {number} tick - Description of tick parameter
  * @return {object} Description of returned value
  */
-ImageZoomNew.prototype.public = function(tick) {
-	return { tock: tick };
+Lightbox.prototype.public = function(tick) {
+
 };
 
 /**
@@ -38,6 +37,6 @@ ImageZoomNew.prototype.public = function(tick) {
  * @private
  * @return {boolean} Description of return value
  */
-ImageZoomNew.prototype._private = function () {
-	return true;
+Lightbox.prototype._private = function () {
+
 };
